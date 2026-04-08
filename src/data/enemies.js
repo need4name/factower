2a0db2e2fcebbc60911656c7509e184d3940e215
need4name <samcroft6@yahoo.co.uk>
2026-04-08 14:34:57 +0100

    Add enemy data for various enemy types

---
+++ src/data/enemies.js
@@ -0,0 +1,45 @@
+const ENEMY_DATA = {
+  saltChild: {
+    key: 'saltChild',
+    name: 'SALT CHILD',
+    hp: 30,
+    speed: 130,
+    partsReward: 2,
+    baseDamage: 1,
+    colour: 0xc8d0dc,
+    size: 10
+  },
+  scrapRunner: {
+    key: 'scrapRunner',
+    name: 'SCRAP RUNNER',
+    hp: 20,
+    speed: 160,
+    partsReward: 3,
+    baseDamage: 0,
+    stealsNuts: true,
+    colour: 0xe8a020,
+    size: 10
+  },
+  driftwoodHulk: {
+    key: 'driftwoodHulk',
+    name: 'DRIFTWOOD HULK',
+    hp: 200,
+    speed: 45,
+    partsReward: 12,
+    baseDamage: 3,
+    splashImmune: true,
+    colour: 0x6a7585,
+    size: 18
+  },
+  frontCommander: {
+    key: 'frontCommander',
+    name: 'FRONT COMMANDER',
+    hp: 400,
+    speed: 60,
+    partsReward: 30,
+    baseDamage: 5,
+    isBoss: true,
+    colour: 0xc43a3a,
+    size: 22
+  }
+};

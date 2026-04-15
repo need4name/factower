4b38e958fbb567832c625ed9e3a5acade3ef5ea1
need4name <samcroft6@yahoo.co.uk>
2026-04-15 21:45:56 +0100

    Update enemies.js

--- src/data/enemies.js
+++ src/data/enemies.js
@@ -1,43 +1,22 @@
 const ENEMY_DATA = {
   saltChild: {
-    key: 'saltChild',
-    name: 'SALT CHILD',
-    hp: 40,
-    speed: 120,
-    partsReward: 2,
-    baseDamage: 2,
-    colour: 0xc8d0dc,
-    size: 10
+    key: 'saltChild', name: 'SALT CHILD',
+    hp: 50, speed: 120, partsReward: 2, baseDamage: 2,
+    colour: 0xc8d0dc, size: 10
   },
   scrapRunner: {
-    key: 'scrapRunner',
-    name: 'SCRAP RUNNER',
-    hp: 25,
-    speed: 180,
-    partsReward: 3,
-    baseDamage: 1,
-    colour: 0xe8a020,
-    size: 9
+    key: 'scrapRunner', name: 'SCRAP RUNNER',
+    hp: 35, speed: 195, partsReward: 3, baseDamage: 1,
+    colour: 0xe8a020, size: 9
   },
   driftwoodHulk: {
-    key: 'driftwoodHulk',
-    name: 'DRIFTWOOD HULK',
-    hp: 300,
-    speed: 40,
-    partsReward: 15,
-    baseDamage: 4,
-    colour: 0x6a7585,
-    size: 18
+    key: 'driftwoodHulk', name: 'DRIFTWOOD HULK',
+    hp: 380, speed: 40, partsReward: 15, baseDamage: 4,
+    colour: 0x6a7585, size: 18
   },
   frontCommander: {
-    key: 'frontCommander',
-    name: 'FRONT COMMANDER',
-    hp: 500,
-    speed: 55,
-    partsReward: 40,
-    baseDamage: 6,
-    isBoss: true,
-    colour: 0xc43a3a,
-    size: 22
+    key: 'frontCommander', name: 'FRONT COMMANDER',
+    hp: 650, speed: 55, partsReward: 40, baseDamage: 6,
+    isBoss: true, colour: 0xc43a3a, size: 22
   }
 };
